9627bc2e1b09279d20c47093f757df033e1d8b1e
WinterP <winterp.dev@gmail.com>
2023-12-20 09:17:10 -0600

    Update

--- Algos/DoublyLinkedList.js
+++ Algos/DoublyLinkedList.js
@@ -74,7 +74,17 @@ class DoublyLinkedList {
      * @returns {DoublyLinkedList} This list.
      */
     insertAtFront(data) {
-        // Create a Node and we want to insert a new head for our list.
+        const newNode = new DLLNode(data)
+        if (this.isEmpty()) {
+            this.head = newNode
+            this.tail = newNode
+            return this
+        }
+        newNode.next = this.head
+
+        this.head.prev = newNode
+        this.head = newNode
+        return this
     }
 
     /**
@@ -86,7 +96,27 @@ class DoublyLinkedList {
      * @returns {DoublyLinkedList} This list.
      */
     insertAtBack(data) {
-        // Create a Node and we want to insert a new tail for our list.
+        const newNode = new DLLNode(data)
+        if (this.isEmpty()) {
+            this.head = newNode
+            this.tail = newNode
+            return this
+        }
+        newNode.prev = this.tail
+
+        this.tail.next = newNode
+        this.tail = newNode
+        return this
+    }
+
+    /**
+     * Adds all the given items to the back of this list.
+     * @param {Array<any>} items Items to be added to the back of this list.
+     * @returns {DoublyLinkedList} This list.
+     */
+    insertAtBackMany(items = []) {
+        items.forEach((item) => this.insertAtBack(item));
+        return this;
     }
 
     /**
@@ -102,13 +132,105 @@ class DoublyLinkedList {
         // Hint: Like when we navigate arrays left and right, we could approach this with two runners. 
         // Example runners: let leftRunner = this.head; let rightRunner = this.tail;
         // If these runners pass each other, it means there is no true middle node. (Such as having a list of 4 Nodes.)
+        if (this.isEmpty()) return null
+        let leftRunner = this.head
+        let rightRunner = this.tail
 
+        while (leftRunner.next != rightRunner) {
+            rightRunner = rightRunner.prev
+            leftRunner = leftRunner.next
+            if (leftRunner === rightRunner) {
+                leftRunner.prev.next = rightRunner.next
+                return leftRunner.data
+            }
+        }
+        return null
     }
 
+    /**
+      * Inserts a new node with the given newVal after the node that has the
+      * given targetVal as it's data.
+      * - Time: O(?).
+      * - Space: O(?).
+      * @param {any} targetVal The node data to find.
+      * @param {any} newVal Data for the new node.
+      * @returns {boolean} Indicates if the new node was added.
+      */
+    insertAfter(targetVal, newVal) {
+        // Your Code Here
+        // Start by checking if the Target Value is the Tail of the List
+        if (this.tail.data == targetVal) {
+            this.insertAtBack(newVal)
+            return true
+        }
+
+        //create a runner to traverse the list from head to tail
+        let runner = this.head
+        //this will stop the runner if the tail is reached and the target value is not discovered
+        while (runner.next != null) {
+            if (runner.data == targetVal) {
+                //build the new node
+                const newNode = new DLLNode(newVal)
+                //set the nodes of newly created node to point to the the values surrouding the runner
+                newNode.prev = runner
+                newNode.next = runner.next
+                //update the next node at the runner's position
+                runner.next = newNode
+                //update the node after the runner's position to reflect the new node
+                runner.next.prev = newNode
+                // return a boolean to confirm this was completed correctly
+                return true
+            }
+            runner = runner.next
+        }
+        return false
+    }
+    /**
+     * Inserts a new node with the given newVal before the node that has the
+     * given targetVal as it's data.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} targetVal The node data to find.
+     * @param {any} newVal Data for the new node.
+     * @returns {boolean} Indicates if the new node was added.
+     */
+    insertBefore(targetVal, newVal) {
+        // Your Code Here
+        if (this.head.data == targetVal) {
+            this.insertAtFront(newVal)
+            return true
+        }
+
+        //create a runner to traverse the list from tail to head
+        let runner = this.tail
+        //this will stop the runner if the tail is reached and the target value is not discovered
+        while (runner.prev != null) {
+            if (runner.data == targetVal) {
+                //build the new node
+                const newNode = new DLLNode(newVal)
+                //set the nodes of newly created node to point to the the values surrouding the runner
+                newNode.next = runner
+                newNode.prev = runner.prev
+                //update the next node at the runner's position
+                //update the node after the runner's position to reflect the new node
+                runner.prev.next = newNode
+                runner.prev = newNode
+                // return a boolean to confirm this was completed correctly
+                return true
+            }
+            runner = runner.prev
+        }
+        return false
+    }
 
 }
 
 const emptyList = new DoublyLinkedList();
+
+const testList = new DoublyLinkedList().insertAtBack(1).insertAtBack(2).insertAtBack(3).insertAtBack(4).insertAtBack(5);
+
+console.log(testList.removeMiddleNode());
+console.log(testList.toArray());
 // const singleNodeList = new DoublyLinkedList().insertAtFront(1);
 // const biNodeList = new DoublyLinkedList().insertAtBack(1).insertAtBack(2);
 // const firstThreeList = new DoublyLinkedList().insertAtBackMany([1, 2, 3]);
---
+++ Algos/Queues.js
@@ -0,0 +1,216 @@
+/**
+ * Class to represent a queue using an array to store the queued items.
+ * Follows a FIFO (First In First Out) order where new items are added to the
+ * back and items are removed from the front.
+ */
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    /**
+     * Retrieves the size of this queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    len() {
+        return this.items.length;
+    }
+
+    /**
+     * Adds a new given item to the back of this queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} item The new item to add to the back.
+     * @returns {number} The new size of this queue.
+     */
+    enqueue(item) {
+        this.items.push(item);
+        return this.items.length;
+    }
+
+    /**
+     * Removes and returns the first item of this queue.
+     * - Time: O(n) linear, due to having to shift all the remaining items to
+     *    the left after removing first elem.
+     * - Space: O(1) constant.
+     * @returns {any} The first item or undefined if empty.
+     */
+    dequeue() {
+        return this.items.shift();
+    }
+
+    /**
+     * Retrieves the first item without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The first item or undefined if empty.
+     */
+    front() {
+        return this.items[0];
+    }
+
+    /**
+     * Returns whether or not this queue is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    print() {
+        console.log(this.items);
+        return this.items;
+    }
+}
+
+/* Rebuild the above class using a linked list instead of an array. */
+
+/* 
+In order to maintain an O(1) enqueue time complexity like .push with an array
+We add a tail to our linked list so we can go directly to the last node
+*/
+
+class QueueNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedListQueue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    /**
+     * Retrieves the size of this queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    len() {
+        return this.size
+    }
+
+    /**
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean} Indicates if the list is empty.
+     */
+    isEmpty() {
+        return this.size === 0
+
+    }
+
+    /**
+     * Adds a given val to the back of the queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} val
+     * @returns {number} The new size of the queue.
+     */
+    enqueue(val) {
+        const newNode = new QueueNode(val);
+
+        if (this.head === null) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+
+        this.size++
+        return this.size;
+
+    }
+
+    /**
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The removed item.
+     */
+    dequeue() {
+        if (!this.head) {
+            return null
+        }
+        const removedHead = this.head
+        //your code here
+        this.head = this.head.next
+        if (this.head === null) {
+            this.tail = null
+        }
+        this.size--
+        return removedHead.data
+    }
+
+    /**
+     * Retrieves the first item without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The first item.
+     */
+    front() {
+
+        return this.head ? this.head.data : null
+
+    }
+
+    /**
+ * Compares this queue to another given queue to see if they are equal.
+ * Two queues are equal if they are the same size and have the same node values in order.
+ * Use the queue methods we've created.
+ * Use no extra array or objects.
+ * The queues should be returned to their original order when done.
+ * 
+ * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+ *     array queue being O(n).
+ * - Space: O(1) constant.
+ * @param {Queue} q2 The queue to be compared against this queue.
+ * @returns {boolean} Whether all the items of the two queues are equal and
+ *    in the same order.
+ */
+    compareQueues(q2) {
+        //Your Code Here
+        // Note: Our Dequeue method we created returns the data of the node. This can be useful for comparing values.
+        // We also want to make sure the queues are returned to the original order after evaluating. (If we were dequeueing, we'd want to enqueue)
+    }
+
+    /**
+     * Enqueues each of the given items.
+     * - Time: O(n) linear since enqueue is O(1), n = vals.length.
+     * - Space: O(1) constant.
+     * @param {Array<any>} vals
+     */
+    seed(vals) {
+        vals.forEach((val) => this.enqueue(val));
+    }
+
+    print() {
+        let runner = this.head;
+        let vals = "";
+
+        while (runner) {
+            vals += `${runner.data}${runner.next ? ", " : ""}`;
+            runner = runner.next;
+        }
+        console.log(vals);
+        return vals;
+    }
+
+}
+
+
+
+const arrayQueue = new Queue();
+arrayQueue.items = [1, 2, 9, 3, 3, 6];
+arrayQueue.print();
+
+const listQueue = new LinkedListQueue();
+listQueue.seed([1, 2, 9, 3, 3, 6]);
+listQueue.print();
